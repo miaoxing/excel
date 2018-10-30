c5877ea1a041e7874fac653f85c6c710fe452901
twinh <twinhuang@qq.com>
2018-10-30 11:14:02 +0800

    feat: 增加代码提示

---
+++ docs/AutoComplete.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MiaoxingDoc\Excel {
+
+    /**
+     * @property    \Miaoxing\Excel\Service\Excel $excel
+     *
+     * @property    \Miaoxing\Excel\Service\Import $import Import
+     */
+    class AutoComplete
+    {
+    }
+}
+
+namespace {
+
+    /**
+     * @return MiaoxingDoc\Excel\AutoComplete
+     */
+    function wei()
+    {
+    }
+
+    /** @var Miaoxing\Excel\Service\Excel $excel */
+    $excel = wei()->excel;
+
+    /** @var Miaoxing\Excel\Service\Import $import */
+    $import = wei()->import;
+}
